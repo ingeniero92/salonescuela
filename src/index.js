b79a7d9a9fee46cacb94f36e11a0392e6ec3e7d8
ingeniero92 <ingeniero92@gmail.com>
2021-10-29 17:31:52 +0200

    Polyfill

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
 import 'core-js';
 import 'core-js/es/map';
 import 'core-js/es/set';
 import 'core-js/es/array/find';
 import 'core-js/es/array/includes';
 import 'core-js/es/number/is-nan';
+import 'raf';
+import 'raf/polyfill';
+import 'react-app-polyfill/stable';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
 import 'font-awesome/css/font-awesome.min.css';
 
