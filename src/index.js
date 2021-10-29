24a6fac520be0292c2f076b44b233fd280634c3a
ingeniero92 <ingeniero92@gmail.com>
2021-10-29 14:48:08 +0200

    CoreJS

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/fn/array/find';
-import 'core-js/fn/array/includes';
-import 'core-js/fn/number/is-nan';
+import 'core-js';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/array/find';
+import 'core-js/es/array/includes';
+import 'core-js/es/number/is-nan';
 
 import 'font-awesome/css/font-awesome.min.css';
 
