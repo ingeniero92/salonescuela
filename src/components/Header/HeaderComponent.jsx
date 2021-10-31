423111b2a768d5444b651a637cb6cca76a8a7538
ingeniero92 <ingeniero92@gmail.com>
2021-10-31 12:58:11 +0100

    Header Menu Done

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { observer } from "mobx-react";
 import * as Scroll from 'react-scroll';
-import { Link } from 'react-scroll';
 
 import { withStore } from "../../providers/StoreRootProvider";
 import BaseComponent from '../Base/BaseComponent';
+import MenuLinksComponent from '../Menu/MenuLinksComponent';
 
 var scroll = Scroll.animateScroll;
 
@@ -16,22 +16,18 @@ class Header extends BaseComponent {
 
     render(){
         return(
-            <React.Fragment>
-                <header>
-                    <div className="content">
-                        <div className="links">
-                            <Link to="school"      spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">ESCUELA</span></Link>
-                            <Link to="program"     spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">PROGRAMA</span></Link>
-                            <Link to="teachers"    spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">TUTORES</span></Link>
-                            <Link to="admission"   spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">ADMISIÓN</span></Link>
-                            <Link to="news"        spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">NOTICIAS</span></Link>
-                            <Link to="contact"     spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">CONTACTO</span></Link>
-                            <Link to="scholarship" spy={true} smooth={true} offset={-200} onClick={() => this.store.appStore.closeModal()} ><span className="link">BECA</span></Link>
-                        </div>
-                        <i onClick={ () => this.store.appStore.toggleModal()} className={this.store.appStore.modalIsOpen ? "fas fa-times" : "fas fa-bars"}></i>
-                    </div>                    
-                </header>
-            </React.Fragment>
+            <header>
+                <div className="content">
+                    <div className={this.store.appStore.modalIsOpen ? "menu open" : "menu closed"}>
+                        <MenuLinksComponent />                          
+                    </div>
+                    <div className={this.store.appStore.modalIsOpen ? "links open" : "links closed"}>
+                        <MenuLinksComponent />
+                    </div>
+                    <i onClick={ () => this.store.appStore.toggleModal()} className={this.store.appStore.modalIsOpen ? "fas fa-bars no-visible" : "fas fa-bars visible"}></i>
+                    <i onClick={ () => this.store.appStore.toggleModal()} className={this.store.appStore.modalIsOpen ? "fas fa-times visible" : "fas fa-times no-visible"}></i>
+                </div>                    
+            </header>
         );
     }
 
---
+++ src/components/Menu/MenuLinksComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+const MenuLinksComponent = () => {
+
+    return (      
+        <React.Fragment>
+            <Link to="school"      spy={true} smooth={true} offset={-200} ><span className="link">ESCUELA</span></Link>
+            <Link to="program"     spy={true} smooth={true} offset={-200} ><span className="link">PROGRAMA</span></Link>
+            <Link to="teachers"    spy={true} smooth={true} offset={-200} ><span className="link">PROFESORES</span></Link>
+            <Link to="admission"   spy={true} smooth={true} offset={-200} ><span className="link">ADMISIÓN</span></Link>
+            <Link to="news"        spy={true} smooth={true} offset={-200} ><span className="link">NOTICIAS</span></Link>
+            <Link to="contact"     spy={true} smooth={true} offset={-200} ><span className="link">CONTACTO</span></Link>
+            <Link to="scholarship" spy={true} smooth={true} offset={-200} ><span className="link">BECA</span></Link>
+        </React.Fragment>
+    );    
+
+};
+
+export default MenuLinksComponent;
