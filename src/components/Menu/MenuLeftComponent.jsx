cf24b92062e73546f5b20f2c8bb217794c06a779
ingeniero92 <ingeniero92@gmail.com>
2021-11-03 21:52:24 +0100

    Desktop Style

--- src/components/Menu/MenuLeftComponent.jsx
+++ src/components/Menu/MenuLeftComponent.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { observer } from "mobx-react";
 
-import { useAppStore } from "../../providers/StoreRootProvider";
 import MenuLinksComponent from './MenuLinksComponent';
 
 const MenuLeftComponent = observer(() => {
-
-    const appStore = useAppStore();
 
     return(
         <div className="navbar">
